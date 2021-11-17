e37f0feb618ac5676d75df30934f0f27bf280f2c
pratibhapw23 <pratibha_wabale@persistent.com>
2021-11-18 00:47:15 +0530

    Used @RequestParam annotation

--- springmvcapplication/src/main/java/com/springmvc/ConfigFileApplication.java
+++ springmvcapplication/src/main/java/com/springmvc/ConfigFileApplication.java
@@ -1,10 +1,21 @@
 package com.springmvc;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan({"com.springmvc"})
 public class ConfigFileApplication {
+	
+	@Bean
+	public InternalResourceViewResolver viewResolver()
+	{
+		InternalResourceViewResolver vr=new InternalResourceViewResolver();
+		//vr.setPrefix("");
+		vr.setSuffix(".jsp");
+		return vr;
+	}
 	
 }
--- springmvcapplication/src/main/java/com/springmvc/controllers/AddController.java
+++ springmvcapplication/src/main/java/com/springmvc/controllers/AddController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.springmvc.services.AddService;
@@ -13,15 +14,15 @@ import com.springmvc.services.AddService;
 public class AddController {
 	
 	@RequestMapping("/add")
-	public ModelAndView add(HttpServletRequest request, HttpServletResponse response)
+	public ModelAndView add(@RequestParam("t1")int num1, @RequestParam("t2")int num2,HttpServletRequest request, HttpServletResponse response)
 	{
-		int num1=Integer.parseInt(request.getParameter("t1"));
-		int num2=Integer.parseInt(request.getParameter("t2"));
+		//int num1=Integer.parseInt(request.getParameter("t1"));
+		//int num2=Integer.parseInt(request.getParameter("t2"));
 		AddService as=new AddService();
 		int add=as.add(num1, num2);
 		//request.setAttribute("addition", add);
 		ModelAndView mv=new ModelAndView();
-		mv.setViewName("display.jsp");
+		mv.setViewName("display");
 		mv.addObject("addition",add);
 		return mv;
 	}
