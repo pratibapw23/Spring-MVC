3d1fe777e0a54a1106e35ad67b4edf14c30ae6f7
pratibhapw23 <pratibha_wabale@persistent.com>
2021-11-19 12:40:39 +0530

    Tested Save Operation on Database

---
+++ UserContactManagement-SpringMVC/src/test/java/com/capp/test/TestUserDAOSave.java
@@ -0,0 +1,35 @@
+package com.capp.test;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.capp.config.SpringRootConfig;
+import com.capp.dao.UserDAO;
+import com.capp.domain.User;
+
+public class TestUserDAOSave {
+	
+	@Test
+	public void TestUserDAOSaveOperation()
+	{
+		ApplicationContext ctx=new AnnotationConfigApplicationContext(SpringRootConfig.class);
+		UserDAO userdao=ctx.getBean(UserDAO.class);
+		
+		User u=new User();
+		u.setName("Neha");
+		u.setPhone("3646336337");
+		u.setEmail("neha@gmail.com");
+		u.setAddress("Indapur");
+		u.setLoginName("Neha2");
+		u.setPassword("Neha1234");
+		u.setRole(1);
+		u.setLoginStatus(1);
+		userdao.save(u);
+		
+		System.out.println("-------Data Saved----");
+		
+		
+	}
+
+}
