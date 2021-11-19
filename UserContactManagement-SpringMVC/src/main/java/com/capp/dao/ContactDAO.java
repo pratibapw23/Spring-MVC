94a7a9de176d9f30e27e57bfe146dd9e7063a57b
pratibhapw23 <pratibha_wabale@persistent.com>
2021-11-19 19:44:34 +0530

    Changed Fetch single user data- method

--- UserContactManagement-SpringMVC/src/main/java/com/capp/dao/ContactDAO.java
+++ UserContactManagement-SpringMVC/src/main/java/com/capp/dao/ContactDAO.java
@@ -11,7 +11,7 @@ public interface ContactDAO {
 	public void update(Contact c);
 	public void delete(Contact c);
 	public void delete(Integer contactid);
-	public User findById(Integer contactid);
+	public Contact findById(Integer contactid);
 	public List<Contact> findAll();
 	public List<Contact> findByProperty(String propName, Object propValue);
 
