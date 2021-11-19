617516f3d05a578ac7880c940af35cd80d164811
pratibhapw23 <pratibha_wabale@persistent.com>
2021-11-19 15:09:10 +0530

    Test Fetch all records from table functionality

---
+++ UserContactManagement-SpringMVC/src/test/java/com/capp/test/TestUserDAOFindAll.java
@@ -0,0 +1,34 @@
+package com.capp.test;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.capp.config.SpringRootConfig;
+import com.capp.dao.UserDAO;
+import com.capp.domain.User;
+
+public class TestUserDAOFindAll {
+	
+	@Test
+	public void TestUserDAOFetchAllUsersDataOperation()
+	{
+		ApplicationContext ctx=new AnnotationConfigApplicationContext(SpringRootConfig.class);
+		UserDAO userdao=ctx.getBean(UserDAO.class);
+		System.out.println("-------Data Fetched----");
+		List<User> users=userdao.findAll();
+		for(User u:users)
+		{
+			System.out.println(u.getUserid());
+			System.out.println(u.getName());
+			System.out.println(u.getAddress());
+		}
+		
+	
+		
+		
+	}
+
+}
