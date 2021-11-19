c9de6769647b2dbccd9aaa4093767992f0c0f8ca
pratibhapw23 <pratibha_wabale@persistent.com>
2021-11-19 12:39:00 +0530

    Changed Scanning package for testing purpose

--- UserContactManagement-SpringMVC/src/main/java/com/capp/config/SpringRootConfig.java
+++ UserContactManagement-SpringMVC/src/main/java/com/capp/config/SpringRootConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-//@ComponentScan(basePackages= {"com.capp"})
+@ComponentScan(basePackages= {"com.capp.dao"})
 public class SpringRootConfig {
 	
 	@Bean
