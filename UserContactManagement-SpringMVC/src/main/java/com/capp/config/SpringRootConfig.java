0a18162c13471c31a0bc1e5fbec1d4f14b2f4557
pratibhapw23 <pratibha_wabale@persistent.com>
2021-11-19 11:24:36 +0530

    DataSource- Implemented Data Source, created database connection

--- UserContactManagement-SpringMVC/src/main/java/com/capp/config/SpringRootConfig.java
+++ UserContactManagement-SpringMVC/src/main/java/com/capp/config/SpringRootConfig.java
@@ -1,10 +1,28 @@
 package com.capp.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages= {"com.capp"})
+//@ComponentScan(basePackages= {"com.capp"})
 public class SpringRootConfig {
+	
+	@Bean
+	public BasicDataSource getDataSource()
+	{
+		BasicDataSource ds=new BasicDataSource();
+		ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		ds.setUrl("jdbc:mysql://localhost:3306/contactapp_db");
+		ds.setUsername("root");
+		ds.setPassword("root");
+		ds.setMaxTotal(2);//Connections that can be active at same time
+		ds.setInitialSize(1);
+		ds.setTestOnBorrow(true);
+		ds.setValidationQuery("SELECT 1");//Test the connection
+		ds.setDefaultAutoCommit(true);
+		return ds;
+	}
 
 }
