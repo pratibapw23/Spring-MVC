d91d4ea496bcbf89c7a2cc73bf96c02cda4050fc
pratibhapw23 <pratibha_wabale@persistent.com>
2021-11-20 12:12:44 +0530

    Added Packages to scan

--- UserContactManagement-SpringMVC/src/main/java/com/capp/config/SpringRootConfig.java
+++ UserContactManagement-SpringMVC/src/main/java/com/capp/config/SpringRootConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages= {"com.capp.dao"})
+@ComponentScan(basePackages= {"com.capp.dao","com.capp.service"})
 public class SpringRootConfig {
 	
 	@Bean
