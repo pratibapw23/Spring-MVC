0f9ba7cd77c1d7fb93d38d85b58b2f91ae5bb12f
pratibhapw23 <pratibha_wabale@persistent.com>
2021-11-22 17:19:08 +0530

    Created Registration form and mapped fields

---
+++ UserContactManagement-SpringMVC/src/main/java/com/capp/command/UserCommand.java
@@ -0,0 +1,20 @@
+package com.capp.command;
+
+import com.capp.domain.User;
+
+public class UserCommand {
+	
+	User user;
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
+	
+
+}
