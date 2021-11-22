223289b75ddc03cc1a996237ea4344bb97d549c2
pratibhapw23 <pratibha_wabale@persistent.com>
2021-11-22 17:21:31 +0530

    Added register request handlers

--- UserContactManagement-SpringMVC/src/main/java/com/capp/controller/UserController.java
+++ UserContactManagement-SpringMVC/src/main/java/com/capp/controller/UserController.java
@@ -1,9 +1,9 @@
 package com.capp.controller;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.capp.command.LoginCommand;
+import com.capp.command.UserCommand;
 import com.capp.domain.User;
 import com.capp.exception.BlockedUserException;
 import com.capp.service.UserService;
@@ -28,7 +29,7 @@ public class UserController {
 		return "index";
 	}
 	@RequestMapping(value="/login", method=RequestMethod.POST)
-	public String HandleLoginPage(@ModelAttribute("command") LoginCommand cmd,  Model m)
+	public String HandleLoginPage(@ModelAttribute("command") LoginCommand cmd,  Model m, HttpSession session)
 	{
 		try {
 		User loggedInUser=userservice.login(cmd.getLoginName(), cmd.getPassword());
@@ -39,9 +40,11 @@ public class UserController {
 		}
 		else {
 			if(loggedInUser.getRole().equals(userservice.Admin_Role)) {
+				addLoggedInUserInSession(loggedInUser,session);
 				return "redirect:admin/dashboard";
 			}
 			else if(loggedInUser.getRole().equals(userservice.User_Role)){
+				addLoggedInUserInSession(loggedInUser,session);
 				return "redirect:user/dashboard";
 			}else {
 				m.addAttribute("err","Invalid User Role");
@@ -64,6 +67,41 @@ public class UserController {
 	public String adminDashboard()
 	{
 		return "dashboard_admin";
+	}
+	@RequestMapping(value="/logout")
+	public String logout(HttpSession session)
+	{
+		session.invalidate();
+		return "redirect:index?act=lo";
+	}
+	
+	@RequestMapping(value="/reg_form")
+	public String registerForm(Model m) {
+		UserCommand cmd=new UserCommand();
+		m.addAttribute("command",cmd);
+		return "reg_form";
+	}
+	@RequestMapping(value="/register")
+	public String registerUser(@ModelAttribute("command") UserCommand cmd,Model m) {
+		try {
+		User user=cmd.getUser();
+		user.setRole(UserService.User_Role);
+		user.setLoginStatus(UserService.loginStatusActive);
+		
+		userservice.register(user);
+		return "redirect:index?act=reg";
+		}
+		catch(DuplicateKeyException e) {
+			e.printStackTrace();
+			m.addAttribute("err","Username is already registered. Please enter another username!");
+			return "reg_form";
+		}
+	}
+	
+	private void addLoggedInUserInSession(User u, HttpSession session) {
+		session.setAttribute("user", u);
+		session.setAttribute("userid", u.getUserid());
+		session.setAttribute("role", u.getRole());
 	}
 
 }
